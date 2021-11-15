911982cec877df86dd3d402514b72772b5fa9e2a
glitcher93 <nigeljdsouza@gmail.com>
2021-11-15 18:38:29 -0500

    switched App.js to App.jsx, initialized the BrowserRouter, Switch, and Route

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route />
+        <Route />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;

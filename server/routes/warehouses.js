9df015c249ae50454add1db697881b482780e953
Royce Co <royce11co@gmail.com>
2021-11-16 17:54:58 -0500

    get api that returns details of specific warehouse given its id

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -3,3 +3,32 @@ const fs = require('fs');
 const router =  express.Router();
 
 module.exports = router;
+
+let warehouseRead = () => {
+    const warehouseParse = JSON.parse(fs.readFileSync('./data/warehouses.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        return data;
+    }));
+    return warehouseParse
+}
+
+router.get('/', (req, res) => {
+    const warehouseData = warehouseRead();
+    res.status(200).json(warehouseData);
+})
+
+router.get('/:warehouseId', (req, res) => {
+    const warehouseData = warehouseRead();
+    const warehouseId = req.params.warehouseId;
+    const foundWarehouse = warehouseData.find(item => item.id === warehouseId);
+    if (!foundWarehouse) {
+        res.status(404).json({
+            error: "Item not found"
+        })
+    }
+    res.status(200).json(foundWarehouse);
+})
+
+module.exports = router;

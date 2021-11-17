e846ac8bc85d14692b9a6e656e926baee98cc59a
Royce Co <royce11co@gmail.com>
2021-11-16 19:50:28 -0500

    patch api to update warehouse information

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -1,33 +1,104 @@
-const express = require('express');
-const fs = require('fs');
-const router =  express.Router();
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
 
-module.exports = router;
+const warehouseFile = path.join(__dirname, "../data/warehouses.json");
 
 let warehouseRead = () => {
-    const warehouseParse = JSON.parse(fs.readFileSync('./data/warehouses.json', 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-        }
-        return data;
-    }));
-    return warehouseParse
+  const warehouseParse = JSON.parse(
+    fs.readFileSync("./data/warehouses.json", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      return data;
+    })
+  );
+  return warehouseParse;
+};
+
+function addWarehouse(body) {
+  const warehouseArr = warehouseRead();
+  const warehouse = new Warehouse(
+    body.name,
+    body.address,
+    body.city,
+    body.country,
+    body.contact
+  );
+  warehouseArr.push(warehouse);
+
+  fs.writeFileSync(warehouseFile, JSON.stringify(warehouseArr));
+
+  return warehouse;
 }
 
-router.get('/', (req, res) => {
-    const warehouseData = warehouseRead();
-    res.status(200).json(warehouseData);
-})
+function Warehouse(name, address, city, country, contact) {
+  this.id = uuidv4();
+  this.name = name;
+  this.address = address;
+  this.city = city;
+  this.country = country;
+  this.contact = contact;
+}
 
-router.get('/:warehouseId', (req, res) => {
+router.get("/", (req, res) => {
+  const warehouseData = warehouseRead();
+  res.status(200).json(warehouseData);
+});
+
+router.get("/:warehouseId", (req, res) => {
+  const warehouseData = warehouseRead();
+  const warehouseId = req.params.warehouseId;
+  const foundWarehouse = warehouseData.find((item) => item.id === warehouseId);
+  if (!foundWarehouse) {
+    res.status(404).json({
+      error: "Warehouse not found",
+    });
+  }
+  res.status(200).json(foundWarehouse);
+});
+
+router.post("/create", (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.country ||
+    !req.body.contact
+  ) {
+    res.status(400).json({
+      error: "POST body must contain all required properties",
+      requiredProperties: ["name", "address", "city", "country", "contact"],
+    });
+  }
+  res.status(200).json(addWarehouse(req.body));
+});
+
+let warehouseUpdate = (data) => {
+    fs.writeFile('./data/warehouses.json', JSON.stringify(data), (error) => {
+        if (error) {
+            console.log(error);
+        }
+        console.log('Warehouse has been updated');
+    })
+}
+
+router.patch('/:warehouseId/update', (req, res) => {
     const warehouseData = warehouseRead();
-    const warehouseId = req.params.warehouseId;
-    const foundWarehouse = warehouseData.find(item => item.id === warehouseId);
+    const foundWarehouse = warehouseData.find(warehouse => req.params.warehouseId === warehouse.id);
     if (!foundWarehouse) {
         res.status(404).json({
-            error: "Item not found"
-        })
+            error: "Warehouse not found"
+        });
     }
+    foundWarehouse.name = req.body.name || foundWarehouse.name;
+    foundWarehouse.address = req.body.address || foundWarehouse.address;
+    foundWarehouse.city = req.body.city || foundWarehouse.city;
+    foundWarehouse.country = req.body.country || foundWarehouse.country;
+    foundWarehouse.contact = req.body.contact || foundWarehouse.name;
+    warehouseUpdate(warehouseData);
     res.status(200).json(foundWarehouse);
 })
 

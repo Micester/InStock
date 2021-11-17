2534ac4ef9648bcfd7b34220ac9c33e8891252c5
glitcher93 <nigeljdsouza@gmail.com>
2021-11-17 11:37:12 -0500

    set up React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,20 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
+    {/* Header Component Goes Here */}
+    <Switch>
+      <Route path='/' exact component /> {/* WarehousePage */}
+      <Route path='/warehouses' exact component /> {/* WarehousePage */}
+      <Route path='/warehouses/:warehouseId' exact component/> {/* WarehouseSinglePage */}
+      <Route path='/warehouses/add/new' exact component /> {/* WarehouseAddPage */}
+      <Route path='/warehouses/:warehouseId/edit' component /> {/* WarehouseEditPage */}
+      <Route path='/inventories' exact component /> {/* InventoryPage */}
+      <Route path='/inventories/:itemId' exact component /> {/* InventorySinglePage */}
+      <Route path='/inventories/add/new' component /> {/* InventoryAddPage */}
+      <Route path='/inventories/:itemId/edit' component /> {/* InventoryEditPage */}
+      {/* If there's time I'll probably do a 404 page and put the route here - Nigel */}
+    </Switch>
+    {/* Footer Component Goes Here */}
     </BrowserRouter>
   );
 }
